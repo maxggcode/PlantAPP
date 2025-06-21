58758c61810ceaec0d37617928c061d55775e918
王宇崴 <chunkaiwang@CHUNdeMacBook-Pro.local>
2025-06-21 16:24:24 +0200

    “建立選單”

--- lib/greenhouse.dart
+++ lib/greenhouse.dart
@@ -8,9 +8,61 @@ class Greenhouse extends StatefulWidget {
 }
 
 class Greenhousestate extends State<Greenhouse> {
+  void add_plant(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          height: 500,
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Text(
+                    "植物品種",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ),
+        );
+      },
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      isScrollControlled: false, // 設為 true 時可撐滿高度
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => add_plant(context),
+        child: Icon(Icons.add)
+        ),
       appBar: AppBar(
         title: Text('溫室'),
         backgroundColor: Colors.grey,
